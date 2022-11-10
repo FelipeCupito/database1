6e3cc7a068d8bba4f8971bbe3ce1068e442263fe
felu98 <65612488+FelipeCupito@users.noreply.github.com>
2022-11-10 18:47:07 -0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # database1
+
+'''
+pasar lo datos:
+scp funciones.sql clientes_banco.csv pagos_cuotas.csv prestamos_banco.csv {user}@pampero.itba.edu.ar:/home/{user}
+
+crear las tablas y triggers:
+
+conctarme a la basedatos:
+psql -h bd1.it.itba.edu.ar -U fcupito PROOF
+
+pasar csv a las tablas:
+\copy clientes_banco(id,dni,telefono,nombre,direccion) from clientes_banco.csv delimiter',' csv header
+
+'''
---
+++ funciones.sql
@@ -0,0 +1,57 @@
+DROP TABLE IF EXISTS clientes_banco cascade ;
+DROP TABLE IF EXISTS prestamos_banco cascade ;
+DROP TABLE IF EXISTS pagos_cuotas;
+DROP TABLE IF EXISTS backup_;
+
+CREATE TABLE clientes_banco
+(
+    id int NOT NULL,
+    dni int not null,
+    telefono char(30),
+    nombre char(100) not null,
+    direccion char(100),
+
+    primary key(id)
+);
+
+CREATE TABLE prestamos_banco(
+    id INT NOT NULL,
+    fecha date not null,
+    cliente_id int not null,
+    importe float not null,
+
+    primary key(id),
+    foreign key (cliente_id) references clientes_banco
+);
+
+CREATE TABLE pagos_cuotas(
+    nro3 INT NOT NULL,
+    prestamo_id int not null,
+    importe int not null,
+    fecha date not null,
+
+    primary key(nro3, prestamo_id),
+    foreign key (prestamo_id) references prestamos_banco
+);
+
+CREATE TABLE backup_(
+    dni int not null,
+    telefono int not null,
+    nombre char(50) not null,
+    nro_prestamos char(100) not null,
+    total_prestamo float not null,
+    total_pago float not null,
+    deudor int not null, ---0 false, 1 true
+
+    primary key(dni)
+);
+
+
+
+
+*/
+
+
+
+
+
